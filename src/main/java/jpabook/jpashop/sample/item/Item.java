da5554754f5ff12ec3645c245e090a7f1b6bd48f
Nayeong0928 <qoskdud0928@gmail.com>
2024-10-28 11:48:36 +0900

    상품 도메인 추가

--- src/main/java/jpabook/jpashop/sample/item/Item.java
+++ src/main/java/jpabook/jpashop/sample/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.sample.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.sample.Category;
+import jpabook.jpashop.sample.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories=new ArrayList<>();
+
+    /**
+     * 재고 증가
+     * @param quantity
+     */
+    public void addStockQuantity(int quantity){
+        this.stockQuantity+=quantity;
+    }
+
+    /**
+     * 재고 감소
+     * @param quantity
+     */
+    public void removeStock(int quantity){
+        int restStock=this.stockQuantity-quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity=restStock;
+    }
 }
